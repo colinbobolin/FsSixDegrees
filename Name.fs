b5e225dd355225d12ff846c357b9bc9e88952bcf
Colin Scott <colinjscott@protonmail.com>
2021-06-10 00:57:54 -0500

    load name table using sqlite import

--- Name.fs
+++ Name.fs
@@ -1,96 +1,56 @@
 namespace SixDegrees
 
+module Sql =
+    open System.IO
+
+    let CreateSqlScript (importFile: string) (createTableSql: string) =
+        let tempFile = Path.GetTempFileName()
+
+        // let sqlFile =
+        //     __SOURCE_DIRECTORY__ + @"\Scripts\importNames.sql"
+
+        let importFileWithEscapedChars = importFile.Replace(@"\", @"\\")
+
+        let sql =
+            sprintf
+                "
+%s
+.mode tabs
+.import %s name"
+                createTableSql
+                importFileWithEscapedChars
+
+        File.WriteAllText(tempFile, sql)
+
+        tempFile
+
+
 module Name =
 
-    open System
-    open System.IO
-    open System.Data.SQLite
-    open SixDegrees
+    open System.Diagnostics
 
-    type Name =
-        { Nconst: string
-          PrimaryName: string
-          BirthYear: string
-          DeathYear: string
-          PrimaryProfession: string
-          KnownForTitles: string }
+    let ExecuteImportCommand =
+        let namesFile =
+            __SOURCE_DIRECTORY__ + @"\Data\name-basics.tsv"
 
-    let fromString (row: string) =
-        let rowParts = row.Split "	"
+        let sqliteFile =
+            __SOURCE_DIRECTORY__ + @"\Data\imdb.sqlite"
 
-        let nconst = rowParts.[0]
-        let primaryName = rowParts.[1]
-        let birthYear = rowParts.[2]
-        let deathYear = rowParts.[3]
-        let primaryProfession = rowParts.[4]
-        let knownForTitles = rowParts.[5]
+        let createTableSql = "
+CREATE TABLE IF NOT EXISTS name (
+    nconst text,
+    primaryName text,
+    birthYear text,
+    deathYear text,
+    primaryProfession text,
+    knownForTitles text
+);"
 
-        { Nconst = nconst
-          PrimaryName = primaryName
-          BirthYear = birthYear
-          DeathYear = deathYear
-          PrimaryProfession = primaryProfession
-          KnownForTitles = knownForTitles }
+        let command =
+            Sql.CreateSqlScript namesFile createTableSql
+            |> sprintf "sqlite3.exe %s < %s" sqliteFile
 
-    let ParseFile file =
-        File.ReadAllLines file
-        |> Array.skip 1
-        |> Array.map (fun row -> fromString row)
+        let procStartInfo =
+            new ProcessStartInfo(FileName = "CMD.exe", Arguments = @"/c" + command)
 
-    let AddNamesToTable names =
-        let connectionString =
-            "Data Source="
-            + __SOURCE_DIRECTORY__
-            + @"\Data\imdb.db;Version=3;"
-
-        use connection = new SQLiteConnection(connectionString)
-
-        SQLiteConnection.CreateFile(@"Data\imdb.db")
-
-        connection.Open()
-
-        let createTable =
-            "CREATE TABLE name ("
-            + "nconst text,"
-            + "primaryName text,"
-            + "birthYear text,"
-            + "deathYear text,"
-            + "primaryProfession text,"
-            + "knownForTitles text"
-            + ");"
-
-        let createCommand =
-            new SQLiteCommand(createTable, connection)
-
-        createCommand.ExecuteNonQuery()
-        |> printfn "Created table: %i"
-
-        let insertSql =
-            "INSERT INTO name(nconst, primaryName, birthYear, deathYear, primaryProfession, knownForTitles) "
-            + "VALUES (@nconst, @primaryName, @birthYear, @deathYear, @primaryProfession, @knownForTitles)"
-
-        names
-        |> Array.map
-            (fun name ->
-                use command = new SQLiteCommand(insertSql, connection)
-
-                command.Parameters.AddWithValue("@nconst", name.Nconst)
-                |> ignore
-
-                command.Parameters.AddWithValue("@primaryName", name.PrimaryName)
-                |> ignore
-
-                command.Parameters.AddWithValue("@birthYear", name.BirthYear)
-                |> ignore
-
-                command.Parameters.AddWithValue("@deathYear", name.DeathYear)
-                |> ignore
-
-                command.Parameters.AddWithValue("@primaryProfession", name.PrimaryProfession)
-                |> ignore
-
-                command.Parameters.AddWithValue("@knownForTitles", name.KnownForTitles)
-                |> ignore
-
-                command.ExecuteNonQuery())
-        |> Array.sum
+        Process.Start procStartInfo
--- Program.fs
+++ Program.fs
@@ -7,10 +7,6 @@ open System.Data.SQLite
 let main argv =
     if argv.Length > 0 then
         if argv.[0] = "--update" then
-            //recreate the database
-            SQLiteConnection.CreateFile(@"Data\imdb.db")
-
-            //load names file
             let nameUrl =
                 "https://datasets.imdbws.com/name.basics.tsv.gz"
 
@@ -19,37 +15,45 @@ let main argv =
 
             Http.DownloadByteFile nameUrl compressedNameFile
             |> GZip.Decompress
-            |> Name.ParseFile
-            |> Name.AddNamesToTable
-            |> printfn "Added %i names to table"
+            |> ignore
 
-            //load title file
-            let titleUrl =
-                "https://datasets.imdbws.com/title.basics.tsv.gz"
+            Name.ExecuteImportCommand |> ignore
+    //|> (fun proc ->
+    //proc.WaitForExit()
 
-            let compressedTitleFile =
-                __SOURCE_DIRECTORY__
-                + @"\Data\title-basics.tsv.gz"
+    //proc.ExitCode
+    //|> printfn "Update Complete with exit code %i")
+    //|> Name.ParseFile
+    //|> Name.AddNamesToTable
+    //|> printfn "Added %i names to table"
 
-            Http.DownloadByteFile titleUrl compressedTitleFile
-            |> GZip.Decompress
-            |> Title.ParseFile
-            |> Title.AddTitlesToTable
-            |> printfn "Added %i titles to table"
+    //load title file
+    // let titleUrl =
+    //     "https://datasets.imdbws.com/title.basics.tsv.gz"
+
+    // let compressedTitleFile =
+    //     __SOURCE_DIRECTORY__
+    //     + @"\Data\title-basics.tsv.gz"
+
+    // Http.DownloadByteFile titleUrl compressedTitleFile
+    // |> GZip.Decompress
+    // |> Title.ParseFile
+    // |> Title.AddTitlesToTable
+    // |> printfn "Added %i titles to table"
 
 
-            //load principal file
-            let principalsUrl =
-                "https://datasets.imdbws.com/title.principals.tsv.gz"
+    // //load principal file
+    // let principalsUrl =
+    //     "https://datasets.imdbws.com/title.principals.tsv.gz"
 
-            let compressedPrincipalsFile =
-                __SOURCE_DIRECTORY__
-                + @"\Data\title-principals.tsv.gz"
+    // let compressedPrincipalsFile =
+    //     __SOURCE_DIRECTORY__
+    //     + @"\Data\title-principals.tsv.gz"
 
-            Http.DownloadByteFile principalsUrl compressedPrincipalsFile
-            |> GZip.Decompress
-            |> Principal.ParseFile
-            |> Principal.AddPrincipalsToTable
-            |> printfn "Added %i principals to table"
+    // Http.DownloadByteFile principalsUrl compressedPrincipalsFile
+    // |> GZip.Decompress
+    // |> Principal.ParseFile
+    // |> Principal.AddPrincipalsToTable
+    // |> printfn "Added %i principals to table"
 
     0
